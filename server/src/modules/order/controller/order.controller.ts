52e1fcad4d38d68d8e5e7145b1f6dc5f5d9b9d1d
gowtamkumar <gowtampaul0@gmail.com>
2024-05-18 20:02:30 +0600

    sslecommerce init

--- server/src/modules/order/controller/order.controller.ts
+++ server/src/modules/order/controller/order.controller.ts
@@ -7,6 +7,8 @@ import {
   orderValidationSchema,
 } from "../../../validation";
 import { OrderItemEntity } from "../model/order-item.entity";
+import axios from "axios";
+const SSLCommerzPayment = require("sslcommerz-lts");
 
 // @desc Get all Order
 // @route GET /api/v1/Order
@@ -66,6 +68,11 @@ export const createOrder = asyncHandler(async (req: any, res: Response) => {
   const connection = await getDBConnection();
   const validation = orderValidationSchema.safeParse(req.body);
 
+  // SSLCommerzPayment
+  const store_id = "ecomm6648b03fa5d37";
+  const store_passwd = "ecomm6648b03fa5d37@ssl";
+  const is_live = false; //true for live, false for sandbox
+
   if (!validation.success) {
     return res.status(401).json({
       message: validation.error.formErrors,
@@ -74,26 +81,72 @@ export const createOrder = asyncHandler(async (req: any, res: Response) => {
 
   const repository = connection.getRepository(OrderEntity);
   const newOrder = repository.create(validation.data);
-  const save = await repository.save(newOrder);
+  const savedOrder = await repository.save(newOrder);
 
   // // insert order items data
-  if (validation.data.orderItems && save.id) {
+  if (validation.data.orderItems && savedOrder.id) {
     const repoOrderitems = connection.getRepository(OrderItemEntity);
     const newOrderItems = await repoOrderitems.create(
       validation.data.orderItems.map((item) => ({
         totalAmount: +item.totalAmount,
         productId: item.productId,
         qty: item.qty,
-        orderId: save.id,
+        orderId: savedOrder.id,
       }))
     );
     await repoOrderitems.save(newOrderItems);
   }
 
+  // if (savedOrder) {
+  //   const data = {
+  //     total_amount: 100,
+  //     currency: "BDT",
+  //     tran_id: "REF123", // use unique tran_id for each api call
+  //     success_url: "http://localhost:3030/success",
+  //     fail_url: "http://localhost:3030/fail",
+  //     cancel_url: "http://localhost:3030/cancel",
+  //     ipn_url: "http://localhost:3030/ipn",
+  //     shipping_method: "Courier",
+  //     product_name: "Computer.",
+  //     product_category: "Electronic",
+  //     product_profile: "general",
+  //     cus_name: "Customer Name",
+  //     cus_email: "customer@example.com",
+  //     cus_add1: "Dhaka",
+  //     cus_add2: "Dhaka",
+  //     cus_city: "Dhaka",
+  //     cus_state: "Dhaka",
+  //     cus_postcode: "1000",
+  //     cus_country: "Bangladesh",
+  //     cus_phone: "01711111111",
+  //     cus_fax: "01711111111",
+  //     ship_name: "Customer Name",
+  //     ship_add1: "Dhaka",
+  //     ship_add2: "Dhaka",
+  //     ship_city: "Dhaka",
+  //     ship_state: "Dhaka",
+  //     ship_postcode: 1000,
+  //     ship_country: "Bangladesh",
+  //   };
+
+  //   try {
+  //     const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+  //     sslcz.init(data).then((apiResponse: { GatewayPageURL: any }) => {
+  //       // Redirect the user to payment gateway
+  //       let GatewayPageURL = apiResponse.GatewayPageURL;
+  //       res.redirect(GatewayPageURL);
+  //       console.log("Redirecting to: ", GatewayPageURL);
+  //     });
+  //   } catch (error) {
+  //     console.error("Payment initiation error:", error);
+  //     throw error;
+  //   }
+  // }
+
   return res.status(200).json({
     success: true,
     msg: "Create a new Order",
-    data: save,
+    data: savedOrder,
   });
 });
 
