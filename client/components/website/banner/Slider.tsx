b7d91a18107e022d5108454df2d7d5d704e783ce
gowtamkumar <gowtampaul0@gmail.com>
2024-09-02 21:54:05 +0600

    home banner

--- client/components/website/banner/Slider.tsx
+++ client/components/website/banner/Slider.tsx
@@ -7,15 +7,15 @@ import { Navigation } from "swiper/modules";
 import Link from "next/link";
 
 export default function Slider({ banners }: any) {
-  const contentStyle: React.CSSProperties = {
-    margin: 0,
-    height: "400px",
-    color: "black",
-    backgroundColor: "red",
-    lineHeight: "400px",
-    textAlign: "center",
-    background: "black",
-  };
+  // const contentStyle: React.CSSProperties = {
+  //   margin: 0,
+  //   height: "400px",
+  //   color: "black",
+  //   backgroundColor: "red",
+  //   lineHeight: "400px",
+  //   textAlign: "center",
+  //   background: "black",
+  // };
   return (
     <div className="container mx-auto bg-[#F6F6F6]">
       <div className="grid grid-cols-12">
@@ -25,15 +25,15 @@ export default function Slider({ banners }: any) {
             spaceBetween={5}
             slidesPerView={1}
             navigation
-            pagination={{
-              clickable: true,
-              el: `swiper-container swiper-container-testClass`,
-              bulletClass: `swiper-pagination-bullet swiper-pagination-testClass`
-           }}
-             wrapperTag='ul'
-            // scrollbar={{ draggable: true }}
-            // onSwiper={(swiper) => console.log(swiper)}
-            // onSlideChange={() => console.log("slide change")}
+            autoplay
+          // navigation={{
+          //   prevEl: '.prev',
+          //   nextEl: '.next',
+          // }}
+          // wrapperTag="ul"
+          // scrollbar={{ draggable: true }}
+          // onSwiper={(swiper) => console.log(swiper)}
+          // onSlideChange={() => console.log("slide change")}
           >
             {banners?.map(
               ({
@@ -48,22 +48,17 @@ export default function Slider({ banners }: any) {
                 url: string;
               }) => (
                 <SwiperSlide key={image}>
-                  <div className="w-10/12 mx-auto">
-                    <div className="grid grid-cols-2">
-                      <div className="flex items-center bg-local backdrop-blur-sm bg-white/5 ">
-                        <div>
-                          <h1 className="text-4xl">{title}</h1>
-                          <p className="my-4">{description}</p>
-                          <Button size="large" type="primary">
-                            <Link href={`${url ? url : "/shop"}`}>
-                              {" "}
-                              Shop Now
-                            </Link>
-                          </Button>
-                        </div>
+                  <div className="w-10/12 mx-auto h-[50vh]">
+                    <div className="grid grid-cols-2 items-center">
+                      <div className="">
+                        <h1 className="text-4xl">{title}</h1>
+                        <p className="my-4">{description}</p>
+                        <Button size="large" type="primary">
+                          <Link href={`${url ? url : "/shop"}`}>Shop Now</Link>
+                        </Button>
                       </div>
+
                       <Image
-                        style={contentStyle}
                         src={
                           image
                             ? `http://localhost:3900/uploads/${image}`
@@ -74,7 +69,7 @@ export default function Slider({ banners }: any) {
                         width="0"
                         height="0"
                         sizes="100vw"
-                        className="w-full h-auto"
+                        className="w-full h-[50vh]"
                       />
                     </div>
                   </div>
