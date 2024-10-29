7553006ad6e30467e38d022d301955fc9edce79f
gowtamkumar <gowtampaul0@gmail.com>
2024-10-29 17:51:23 +0600

    font,color,fontfamily,layout setup global

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -8,6 +8,33 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        bioxin: {
+          primary: "#F7AA0E",
+          secondary: "#000000",
+          accent: "#F2F2F2",
+        },
+      },
+      fontFamily: {
+        fontfamily: ["Poppins", "Urbanist"],
+        "primary-fontfamily": "Poppins",
+        "secondary-fontfamily": "Urbanist",
+        "bangla-primary-fontfamily": "হিন্দ শিলিগুড়ি",
+        "bangla-secondary-fontfamily": "আদর নইিরত",
+      },
+      fontSize: {
+        "font-size-h1": "1rem",
+        "font-size-h2": "0.8rem",
+        "font-size-h3": "0.8rem",
+        "font-size-h4": "0.8rem",
+        "font-size-h5": "0.8rem",
+        "font-size-h6": "0.8rem",
+        "font-size-p": "0.5rem",
+      },
+      fontWeight: {
+        "weight-100": "100",
+      },
+
       // backgroundImage: {
       //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
       //   "gradient-conic":
