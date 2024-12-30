e3a4b21b5740aec0ef0634c083c7dfd778157fbb
Gowtam kumar <gowtampaul0@gmail.com>
2024-12-30 08:07:12 +0600

    update

--- client/components/share-component/editor-quill/TextQuillEditor.tsx
+++ client/components/share-component/editor-quill/TextQuillEditor.tsx
@@ -1,32 +1,32 @@
-import React from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css"; // Import Quill styles
+// import React from "react";
+// import ReactQuill from "react-quill";
+// import "react-quill/dist/quill.snow.css"; // Import Quill styles
 
-interface TextQuillEditorProps {
-  editorContent: string;
-  // setEditorContent: React.Dispatch<React.SetStateAction<string>>;
-  setEditorContent: any;
-}
+// interface TextQuillEditorProps {
+//   editorContent: string;
+//   // setEditorContent: React.Dispatch<React.SetStateAction<string>>;
+//   setEditorContent: any;
+// }
 
-const TextQuillEditor: React.FC<TextQuillEditorProps> = ({ editorContent, setEditorContent }) => {
-  const handleEditorChange = (content: string) => {
-    setEditorContent(content);
-  };
+// const TextQuillEditor: React.FC<TextQuillEditorProps> = ({ editorContent, setEditorContent }) => {
+//   const handleEditorChange = (content: string) => {
+//     setEditorContent(content);
+//   };
 
-  return (
-    <div>
-      <ReactQuill
-        value={editorContent}
-        onChange={handleEditorChange}
-        theme="snow" // You can choose between 'bubble' or 'snow'
-        placeholder="Start typing here..."
-      />
-      <div style={{ marginTop: "20px" }}>
-        <h3>Editor Output</h3>
-        <div dangerouslySetInnerHTML={{ __html: editorContent }} />
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <ReactQuill
+//         value={editorContent}
+//         onChange={handleEditorChange}
+//         theme="snow" // You can choose between 'bubble' or 'snow'
+//         placeholder="Start typing here..."
+//       />
+//       <div style={{ marginTop: "20px" }}>
+//         <h3>Editor Output</h3>
+//         <div dangerouslySetInnerHTML={{ __html: editorContent }} />
+//       </div>
+//     </div>
+//   );
+// };
 
-export default TextQuillEditor;
+// export default TextQuillEditor;
