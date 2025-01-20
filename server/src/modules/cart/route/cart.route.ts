0d44057321889d27c558bc0c353f26e7ee4d1e29
Gowtam kumar <gowtampaul0@gmail.com>
2025-01-20 21:38:07 +0600

    some upate

--- server/src/modules/cart/route/cart.route.ts
+++ server/src/modules/cart/route/cart.route.ts
@@ -18,7 +18,6 @@ router.route("/user").get(getCartByUser);
 router.route("/list").get(getCartList);
 
 router.route("/qty-up-down/:id").put(AuthGuard, cartIncrementDecrement);
-
 router.route("/:id").get(getCart).patch(updateCart).delete(deleteCart);
 
 export default router;
