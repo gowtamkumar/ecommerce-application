f6d09454b855ad8662727462071137bef46d4184
gowtamkumar <gowtampaul0@gmail.com>
2025-02-15 19:49:49 +0600

    server update

--- server/index.ts
+++ server/index.ts
@@ -18,8 +18,6 @@ const app = express();
 
 // access public folder for image
 if (process.env.NODE_ENV === "development") {
-  console.log("ddd", path.join(__dirname, "public"))
-  
   app.use(express.static(path.join(__dirname, "public")));
 }
 
