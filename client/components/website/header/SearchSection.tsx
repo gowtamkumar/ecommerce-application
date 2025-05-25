c1b75f4493eecd89b8c4b42f87a4ebc2f049494d
gowtamkumar <gowtampaul0@gmail.com>
2025-05-25 14:06:03 +0600

    header desing update

--- client/components/website/header/SearchSection.tsx
+++ client/components/website/header/SearchSection.tsx
@@ -13,16 +13,16 @@ export default function SearchSection() {
     <div className="container mx-auto items-center py-1">
       <div className="grid md:grid-cols-12 justify-between items-center">
         {!global.mobile && (
-          <div className="col-span-2">
+          <div className="col-span-3">
             <Logo />
           </div>
         )}
 
-        <div className="col-span-8 px-5">
+        <div className="col-span-6 px-5">
           <SearchEngine />
         </div>
         {!global.mobile && (
-          <div className="col-span-2">
+          <div className="col-span-3">
             <div className="flex md:gap-4 gap-1 justify-end items-center order-3 px-2">
               <HeaderRight />
             </div>
