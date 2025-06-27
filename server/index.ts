a7889efe975ce39c01af2e4a261cef565e5a3e2d
gowtamkumar <gowtampaul0@gmail.com>
2025-06-27 13:13:25 +0600

    server update

--- server/index.ts
+++ server/index.ts
@@ -26,6 +26,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(express.static(path.join(__dirname, "public")));
 }
 
+
 if (process.env.NODE_ENV === "production") {
   app.use(
     express.static(path.join(__dirname, "..", "public"), {
