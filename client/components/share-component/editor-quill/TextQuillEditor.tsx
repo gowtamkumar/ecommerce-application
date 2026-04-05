8e467e2d2cf57c7ae26ab2bde011385cfdfece6d
gowtamkumar <gowtampaul0@gmail.com>
2026-04-05 11:15:03 +0600

    npm packge update fix

--- client/components/share-component/editor-quill/TextQuillEditor.tsx
+++ client/components/share-component/editor-quill/TextQuillEditor.tsx
@@ -1,32 +1,31 @@
-// "use client";
-// import dynamic from 'next/dynamic';
-// import React from 'react';
-// // import ReactQuill from 'react-quill';
-// // import 'react-quill/dist/quill.snow.css';
+"use client";
+import dynamic from 'next/dynamic';
+import React from 'react';
+import 'react-quill/dist/quill.snow.css';
 
-// interface TextQuillEditorProps {
-//   editorContent: string;
-//   setEditorContent: any;
-// }
-// const ReactQuill = dynamic(() => import('react-quill'), {
-//   ssr: false,
-// });
+interface TextQuillEditorProps {
+  editorContent: string;
+  setEditorContent: any;
+}
+const ReactQuill = dynamic(() => import('react-quill'), {
+  ssr: false,
+});
 
-// const TextQuillEditor: React.FC<TextQuillEditorProps> = ({ editorContent, setEditorContent }) => {
-//   const handleEditorChange = (content: string) => {
-//     setEditorContent(content);
-//   };
+const TextQuillEditor: React.FC<TextQuillEditorProps> = ({ editorContent, setEditorContent }) => {
+  const handleEditorChange = (content: string) => {
+    setEditorContent(content);
+  };
 
-//   return (
-//     <div>
-//       <ReactQuill
-//         value={editorContent}
-//         onChange={handleEditorChange}
-//         theme="snow"
-//         placeholder="Start typing here..."
-//       />
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <ReactQuill
+        value={editorContent}
+        onChange={handleEditorChange}
+        theme="snow"
+        placeholder="Start typing here..."
+      />
+    </div>
+  );
+};
 
-// export default TextQuillEditor;
+export default TextQuillEditor;
